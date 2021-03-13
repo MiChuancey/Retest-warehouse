f9d2b3f49f4d335fece0f97d6b3e30cfc486531d
Chuancey <37216768+MiChuancey@users.noreply.github.com>
2021-03-13 09:25:20 +0800

    create thread

--- linux_system_program/systemProgramme/thread/posix/main.c
+++ linux_system_program/systemProgramme/thread/posix/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+void* hello (void* arg){
+    puts("thread is woking");
+}
 
 int main() {
     printf("Hello, World!\n");
+
+    pthread_t tid;
+    int retNum = pthread_create(&tid,NULL,hello,NULL);
+    if(retNum!=0) {
+        fprintf(stderr,"pthread_create %s\n",strerror(retNum));
+        exit(1);
+    }
+    puts("over");
     return 0;
 }
